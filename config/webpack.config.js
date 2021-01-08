402c788533d1442d299c7c848923374f72cfe71c
YukiSato <yukiyukiamam@gmail.com>
2021-01-08 22:05:02 +0900

    [fix]error

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -129,7 +129,7 @@ module.exports = function(webpackEnv) {
   };
 
   return {
-    mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development',
+    mode: isEnvProduction ? 'development' : isEnvDevelopment && 'development',
     // Stop compilation early in production
     bail: isEnvProduction,
     devtool: isEnvProduction
