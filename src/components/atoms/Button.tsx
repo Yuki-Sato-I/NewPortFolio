3cac4f56b67d6ee6cbb0b9105fff53627344e3e7
YukiSato <yukiyukiamam@gmail.com>
2021-01-09 14:22:37 +0900

    [fix]error

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -23,7 +23,6 @@ enum ModifierClassNames {
 const Button: React.FC<ButtonProps> = ({theme = [ButtonThemes.INIT], children, propStyle = {}, onClick}) => {
   const modifierClasses = theme.map(data => ModifierClassNames[data]).join(' ');
   return (
-    //style={propStyle}style直書き
     <button className={["button", modifierClasses].join(' ')} style={propStyle} onClick={onClick}>
       {children}
       <style jsx>
